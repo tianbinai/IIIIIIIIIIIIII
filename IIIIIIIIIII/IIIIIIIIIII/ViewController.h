06c770a8eedae01f222d8aa150495b7dce0ba8ea
qianfeng <qianfeng@bajiepaoliangquan.local>
2015-12-02 20:21:30 +0800

    IIIIIIIII

---
+++ IIIIIIIIIII/IIIIIIIIIII/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  IIIIIIIIIII
+//
+//  Created by qianfeng on 15/12/2.
+//  Copyright (c) 2015年 TB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+-(void)SSSSSSS;
+
+@end
+
---
+++ IIIIIIIIIII/IIIIIIIIIII/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  IIIIIIIIIII
+//
+//  Created by qianfeng on 15/12/2.
+//  Copyright (c) 2015年 TB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)SSSSSSS
+{
+    NSLog(@"HUIUHHUOUK");
+}
+
+
+@end
